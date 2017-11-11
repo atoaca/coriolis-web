cbf9d67b1050a1b994fb27a08d6f409b1a2de3f7
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2017-11-11 18:46:54 +0200

    Add copyright header to all files

--- src/components/molecules/NotificationDropdown/images/bell.js
+++ src/components/molecules/NotificationDropdown/images/bell.js
@@ -1,3 +1,17 @@
+/*
+Copyright (C) 2017  Cloudbase Solutions SRL
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const bell = color => `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" 
 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
