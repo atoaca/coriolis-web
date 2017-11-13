49aa87d4b6a81d45a76f0028126778e067c11ec7
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2017-11-13 16:36:49 +0200

    Add README.md and LICENSE file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
 let isDev = process.argv.find(a => a === '--dev')
 if (isDev) {
